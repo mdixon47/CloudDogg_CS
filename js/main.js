fa9b69567152c9aede46510006d1fec0de3d8cf1
Malik Dixon <mdixon47@users.noreply.github.com>
2023-05-25 10:40:18 -0400

    added images and updated files

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ $(document).ready(function(){
 
     $('.bg-1 .sl-slide-inner , .bg-1 .sl-content-slice').css("background", "url('img/cd_background1.jpg') center").css("background-size", "cover");
     $('.bg-2 .sl-slide-inner , .bg-1 .sl-content-slice').css("background", "url('img/cd_background2.jpg') center").css("background-size", "cover");
-    $('.bg-3 .sl-slide-inner , .bg-1 .sl-content-slice').css("background", "url('img/slide-3.jpg') center").css("background-size", "cover");
+    $('.bg-3 .sl-slide-inner , .bg-1 .sl-content-slice').css("background", "url('img/cd_background3.jpg') center").css("background-size", "cover");
     $('.bg-4 .sl-slide-inner , .bg-1 .sl-content-slice').css("background", "url('img/slide-4.jpg') center").css("background-size", "cover");
     $('.bg-5 .sl-slide-inner , .bg-1 .sl-content-slice').css("background", "url('img/slide-5.jpg') center").css("background-size", "cover");
 
